27d6c0db541fdf56f6398e0c15d97f9d209565bf
Thierry K <thierry.kamagne@gmail.com>
2023-03-01 07:36:49 +0000

    Init

--- test.py
+++ test.py
@@ -1,8 +1,18 @@
 import json
 import pathlib
 import os
-BASE_DIR = pathlib.Path(__file__)
+BASE_DIR = pathlib.Path(__file__).parent.resolve()
 FILES_DIR = f"{BASE_DIR}/files"
 
-print(BASE_DIR)
-print(FILES_DIR)
+python_data = {
+    "user": {
+        "name": " kamil",
+        "age": 43,
+        "Place": "Ethiopia",
+        "gender": "male"
+    }
+}
+# saving Python dictionary object to JSON file
+with open(f"{FILES_DIR}/json_data.json", "w") as file_stream:
+    json.dump(python_data, file_stream)
+    file_stream.write('\n')
